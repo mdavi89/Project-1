b5c7936e1396d9670e1047d75038706f811e48d0
mdavi89 <83469622+mdavi89@users.noreply.github.com>
2024-09-16 09:56:46 -0400

    fixed js for the table

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-
-const submitButton = document.querySelector()
-const expenseInput = document.querySelector()
-const amountInput = document.querySelector()
-const budgetInput = document.querySelector()
+const formEl = document.querySelector('form');
+const clearBtmEl = document.getElementById('clear');
+const expenseInput = document.getElementById('expense');
+const amountInput = document.getElementById('amount');
+const budgetInput = document.getElementById('budget');
 
 
 function getInfo(event) {
   event.preventDefault();
   
-  const expense = expenseInput.value
-  const amount = amountInput.value
-    if (isNaN(amount)) {
-      return;
-    }
-  const budget = budgetInput.value
-    if (isNaN(budget)) {
-      return;
-    }
-  localStorage.setItem('expense', expense);
-  localStorage.setItem('amount', amount);
-  localStorage.setItem('budget', budget);
+  let expDataEntry = {
+    expense: expenseInput.value,
+    amount: amountInput.value,
+  };
+
+  storeLocalStorage(expDataEntry);
+  renderBudgetList();
+  formEl.reset();
+
 }
-submitButton.addEventListener('click', getInfo)
+
+function clearTable(event) {
+  event.preventDefault();
+  tbodyEl.innerHTML = '';
+  localStorage.clear();
+  renderBudgetList();
+};
+
+formEl.addEventListener('submit', getInfo);
+clearBtmEl.addEventListener('click', clearTable);
+
 
